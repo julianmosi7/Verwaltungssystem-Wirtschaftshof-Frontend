247046032c6a21278398cb00b89e1e3cd2f5ee83
Selina-Moshammer <moshammer.selina@htl-grieskirchen.at>
2021-10-08 11:54:40 +0200

    Initial commit / Login & Register window

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './authentication/login/login.component';
+import {RegisterComponent} from './authentication/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  //canActivate: [AuthGuard]
+  path: '', component: HomeComponent},
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent },
+
+// otherwise redirect to home
+{ path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
