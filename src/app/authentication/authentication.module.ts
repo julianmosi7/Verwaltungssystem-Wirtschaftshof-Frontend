a36b615580c2e6b24e46e819d497ce9592e08a2b
Selina-Moshammer <moshammer.selina@htl-grieskirchen.at>
2021-10-11 15:51:58 +0200

    Structure change  - authentication

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {RouterModule} from '@angular/router';
 
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class AuthenticationModule { }
